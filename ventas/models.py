bdb652fd09f76ae716cc3d403ac1b424f7f2ecc3
Diego Saavedra <dsaavedra88@gmail.com>
2023-07-31 10:46:33 -0500

    Update models and database connection

--- ventas/models.py
+++ ventas/models.py
@@ -1,41 +1,45 @@
 from django.db import models
 
 class Cliente(models.Model):
-    
-        nombre = models.CharField(max_length=50)
-        apellido = models.CharField(max_length=50)
-        cédula = models.CharField(max_length=10)
-        dirección = models.CharField(max_length=50)
-        teléfono = models.CharField(max_length=10)
-        correo = models.CharField(max_length=50)
-        fecha = models.DateField(auto_now=True)
-    
-        def __str__(self):
-            return self.nombre + ' ' + self.apellido
-        
-class Producto(models.Model):
-        
-        nombre = models.CharField(max_length=50)
-        descripción = models.CharField(max_length=50)
-        marca = models.CharField(max_length=50)
-        talla = models.CharField(max_length=50)
-        color = models.CharField(max_length=50)
-        material = models.CharField(max_length=50)
-        precio = models.DecimalField(max_digits=10, decimal_places=2)
-        stock = models.IntegerField()
-        fecha = models.DateField(auto_now=True)
-        
-        def __str__(self):
-            return self.nombre + ' ' + self.marca
-
-class Venta(models.Model):
-
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
-    descuento = models.DecimalField(max_digits=10, decimal_places=2)
-    iva = models.DecimalField(max_digits=10, decimal_places=2)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    documento = models.CharField(max_length=20, choices=[('cédula', 'Cédula'), ('ruc', 'RUC'), ('pasaporte', 'Pasaporte')])  # Adjust the max_length for the documento field
+    documento_detalle = models.IntegerField(default=1000000000, verbose_name='Número de cédula')
+    nombre = models.CharField(max_length=50, verbose_name='Nombre del cliente')
+    apellido = models.CharField(max_length=50, verbose_name='Apellido del cliente')
+    dirección = models.CharField(max_length=50, verbose_name='Dirección del cliente')
+    teléfono = models.IntegerField(verbose_name='Teléfono del cliente')
+    email = models.CharField(max_length=50, verbose_name='Correo electrónico del cliente')
 
     def __str__(self):
-        return self.cliente + ' ' + self.producto + ' ' + self.total
+        return self.nombre + ' ' + self.apellido
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name='Nombre del producto')
+    descripción = models.TextField(verbose_name='Descripción del producto')
+    imagen = models.ImageField(upload_to='productos', null=True, blank=True, verbose_name='Imagen del producto')
+    marca = models.CharField(max_length=50, choices=[('nike', 'Nike'), ('adidas', 'Adidas'), ('puma', 'Puma'), ('reebok', 'Reebok')])  # Add max_length for marca field
+    talla = models.CharField(max_length=50, choices=[('s', 'S'), ('m', 'M'), ('l', 'L'), ('xl', 'XL')])  # Add max_length for talla field
+    color = models.CharField(max_length=50, choices=[('negro', 'Negro')])
+    material = models.CharField(max_length=50, choices=[('poliester', 'Poliester')])
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(verbose_name='Stock del producto')
+    fecha = models.DateField(auto_now=True)
+        
+    def __str__(self):
+        return self.nombre + ' ' + self.marca
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    subtotal = models.FloatField(verbose_name='Costo de la prenda')
+    observaciones = models.CharField(max_length=50, verbose_name='Observaciones de la venta')
+    iva = models.FloatField(choices=[(0.12, '12%')])
+    total = models.FloatField()
+    descuento = models.FloatField(choices=[(0.1, '10%'), (0.2, '20%'), (0.3, '30%')])  # Add choices for discount percentages
+
+    @property
+    def total(self):
+        total = self.subtotal + self.iva - (self.subtotal * self.descuento)  # Calculate total amount after discount
+        return total
+
+    def __str__(self):
+        return f"{self.cliente} - {self.producto} por el costo tota de: {self.total}"
--- ventasprendas/settings.py
+++ ventasprendas/settings.py
@@ -74,14 +74,23 @@ WSGI_APPLICATION = 'ventasprendas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'ventasropa',
+        'USER': 'root',
+        'PASSWORD': 'D3mon1o.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
